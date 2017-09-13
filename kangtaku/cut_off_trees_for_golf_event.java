c371bff44fc50a91d38837844f7d2498eae33f11
kangtaku <jackkdw@gmail.com>
2017-09-13 20:18:39 +0900

    accepted 된걸로 코드 교

--- kangtaku/cut_off_trees_for_golf_event.java
+++ kangtaku/cut_off_trees_for_golf_event.java
@@ -1,97 +1,101 @@
 class Solution {
-        int n, m;
-        public int cutOffTree(List<List<Integer>> forest) {
-            n = forest.size();
-            m = forest.get(0).size();
-            int i, j;
-            int[][] cache = new int[n][m];
+    int n, m;
+    int[][] arrow = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    List<List<Integer>> forest;
 
-            int[][] arrow = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-            List<Integer> trees = new ArrayList<>();
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < m; j++) {
-                    cache[i][j] = -1;
-                    if (forest.get(i).get(j) > 1) {
-                        trees.add(forest.get(i).get(j));
-                    }
-                }
-            }
-            trees.sort(Comparator.comparingInt(x -> x));
-            PriorityQueue<Status> q = new PriorityQueue<Status>();
-            q.add(new Status(0, 0, forest, 0, 0));
-
-            while (q.size() != 0) {
-                Status now = q.poll();
-
-                if (now.x == -1 || now.x == n || now.y == -1 || now.y == m) continue;
-                if (now.curVal() == 0) continue;
-                if (cache[now.x][now.y] == -1) {
-                    cache[now.x][now.y] = now.count;
-                } else if (now.curVal() == 1 && forest.get(now.x).get(now.y) > 1 && cache[now.x][now.y] <= now.count) {
-                    continue;
-                }
-
-                if (trees.get(now.left) == now.curVal()) {
-                    now.cut();
-                }
-
-                if (now.left == trees.size()) {
-                    return now.count;
-                }
-
-                for(i = 0; i < 4; i++) {
-                    q.add(new Status(arrow[i], now));
-                }
-            }
-            return -1;
+    public int cutOffTree(List<List<Integer>> forest) {
+      int i, j, ans = 0;
+      PriorityQueue<Tree> pq = new PriorityQueue<>();
+      this.forest = forest;
+      this.n = forest.size();
+      this.m = forest.get(0).size();
+      for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+          if (forest.get(i).get(j) < 2) {
+            continue;
+          }
+          pq.add(new Tree(i, j, forest.get(i).get(j)));
         }
+      }
 
-        class Status implements Comparable<Status> {
-            List<List<Integer>> forest;
-            int x, y;
-            int left;
-            int count;
+      int nowX = 0, nowY = 0, count;
 
-            public Status(int x, int y, List<List<Integer>> forest, int left, int count) {
-                this.forest = newForest(forest);
-                this.x = x;
-                this.y = y;
-                this.left = left;
-                this.count = count;
-            }
-
-            public Status(int[] arrow, Status curStatus) {
-                this.x = curStatus.x + arrow[0];
-                this.y = curStatus.y + arrow[1];
-                this.forest = newForest(curStatus.forest);
-                this.left = curStatus.left;
-                this.count = curStatus.count + 1;
-            }
-
-            public List<List<Integer>> newForest(List<List<Integer>> forest) {
-                List<List<Integer>> copiedForest = new ArrayList<>();
-                for (int i = 0; i < n; i++) {
-                    List<Integer> row = new ArrayList<>();
-                    for (int j = 0; j < m; j++) {
-                        row.add(forest.get(i).get(j));
-                    }
-                    copiedForest.add(row);
-                }
-                return copiedForest;
-            }
-
-            public int curVal() {
-                return this.forest.get(x).get(y);
-            }
-
-            public void cut() {
-                forest.get(x).set(y, 1);
-                left++;
-            }
-
-            @Override
-            public int compareTo(Status o) {
-                return o.left - this.left;
-            }
+      while (pq.size() != 0) {
+        Tree next = pq.poll();
+        count = findNext(nowX, nowY, next);
+        if (count == -1) {
+          ans = -1;
+          break;
         }
+        ans += count;
+        nowX = next.x;
+        nowY = next.y;
+      }
+
+      return ans;
     }
+
+    private int findNext(int nowX, int nowY, Tree next) {
+      Queue<Position> pq = new LinkedList<>();
+      pq.add(new Position(nowX, nowY, 0));
+      int[][] visited = new int[n][m];
+      int i, j;
+
+      while (pq.size() != 0) {
+        Position cur = pq.poll();
+        for (i = 0; i < 4; i++) {
+            int nxtX = cur.x + arrow[i][0];
+            int nxtY = cur.y + arrow[i][1];
+            if (nxtX == -1 || nxtX == n || nxtY == -1 || nxtY == m) continue;
+            if (forest.get(nxtX).get(nxtY) == 0) continue;
+            if (visited[nxtX][nxtY] != 0) continue;
+            if (nxtX == next.x && nxtY == next.y) {
+                return cur.count + 1;
+            }
+            visited[nxtX][nxtY] = 1;
+            pq.add(new Position(arrow[i], cur));
+        }
+      }
+
+      return -1;
+    }
+
+    class Position {
+      int x;
+      int y;
+      int count;
+
+      public Position(int x, int y, int count) {
+        this.x = x;
+        this.y = y;
+        this.count = count;
+      }
+
+      public Position(int[] arrow, Position cur) {
+        this.x = cur.x + arrow[0];
+        this.y = cur.y + arrow[1];
+        this.count = cur.count + 1;
+      }
+
+
+      boolean isArrived(Tree tree) {
+        return this.x == tree.x && this.y == tree.y;
+      }
+    }
+
+
+    class Tree implements Comparable<Tree> {
+      int x, y, treeSize;
+
+      Tree(int x, int y, int treeSize) {
+        this.x = x;
+        this.y = y;
+        this.treeSize = treeSize;
+      }
+
+      @Override
+      public int compareTo(Tree o) {
+        return this.treeSize - o.treeSize;
+      }
+    }
+  }
