da57c10cb13ddbee60aa87163bf0953a4fc4ca10
김민지 <minji@zuminternet.com>
2017-11-13 16:42:11 +0900

    bfs

--- iamminji/leetcode/medium/pacific_atlantic_water_flow.py
+++ iamminji/leetcode/medium/pacific_atlantic_water_flow.py
@@ -73,5 +73,6 @@ class Solution(object):
 
 
 if __name__ == "__main__":
+    # python 2
     sol = Solution()
     print sol.pacificAtlantic([[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]])
