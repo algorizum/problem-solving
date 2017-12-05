182a9cacbc7a06263e3991466c8604b89a83b04a
kangtaku <jackkdw@gmail.com>
2017-12-06 00:36:16 +0900

    modified shorter

--- kangtaku/asteroid_collision.java
+++ kangtaku/asteroid_collision.java
@@ -2,32 +2,18 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Deque<Integer> dq = new LinkedList<>();
         for (int ast : asteroids) {
-            if (ast > 0) {
-                if (dq.size() == 0 || dq.getLast() > 0 || dq.getLast() < 0) {
-                    dq.add(ast);
-                }
-            } else {
-                if (dq.size() == 0 || dq.getLast() < 0) {
-                    dq.add(ast);
-                } else {
-                    while(dq.size() != 0) {
-                        int lastAst = dq.getLast();
-                        if (lastAst < 0 && ast < 0) {
-                            break;
-                        } else if (lastAst < ast * -1) {
-                            dq.pollLast();
-                        } else if (lastAst > ast * -1) { 
-                            ast = 0;
-                            break;
-                        } else if (lastAst == ast * -1) {
-                            ast = 0;
-                            dq.pollLast();
-                            break;
-                        }
-                    }
-                    if (ast != 0) dq.add(ast);
+            while(dq.size() != 0 && ast < 0 && dq.getLast() > 0) {
+                int lastAst = dq.getLast();
+                if (lastAst < ast * -1) {
+                    dq.pollLast();
+                } else if (lastAst > ast * -1) { 
+                    ast = 0;
+                } else if (lastAst == ast * -1) {
+                    ast = 0;
+                    dq.pollLast();
                 }
             }
+            if (ast != 0) dq.add(ast);
         }
         int ansSize = dq.size();
         int[] ans = new int[ansSize];
